d53f55a5acf618a0c96b4db0ef382d9445ed4571
Ayush Bajirao <bajiraoayush@gmail.com>
2021-01-22 04:13:54 -0500

    First commit.

---
+++ cheatsheet.js
@@ -0,0 +1,9 @@
+let myString, myRegex, result;
+
+
+// Basic search in a string.
+myString = "Hello, World!"
+myRegex = /Hello/
+result = myRegex.test(myString)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
