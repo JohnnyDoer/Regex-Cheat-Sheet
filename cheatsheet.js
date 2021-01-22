eef4d3aa0404e28efef20f48146cda4a05c29b41
Ayush Bajirao <bajiraoayush@gmail.com>
2021-01-22 04:19:22 -0500

    Multiple searches.

--- cheatsheet.js
+++ cheatsheet.js
@@ -7,3 +7,19 @@ myRegex = /Hello/
 result = myRegex.test(myString)
 
 console.log(myString + '\n' + myRegex + '\n' + result + '\n')
+
+
+// Basic search in a string 2.
+myString = "Somewhere Waldo is hiding in the text!"
+myRegex = /Waldo/
+result = myRegex.test(myString)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
+
+
+// Searching for either of multiple patterns.
+myString = "James has a pet cat."
+myRegex = /dog|cat|fish|bird/       // Either dog or cat or fish or bird.
+result = myRegex.test(myString)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
