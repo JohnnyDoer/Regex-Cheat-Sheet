dea1bcc1811806299423d813544022ac4477e728
Ayush Bajirao <bajiraoayush@gmail.com>
2021-01-22 04:23:06 -0500

    All basic regexes.

--- cheatsheet.js
+++ cheatsheet.js
@@ -49,3 +49,35 @@ myRegex = /repeat/ig
 result = myString.match(myRegex)
 
 console.log(myString + '\n' + myRegex + '\n' + result + '\n')
+
+
+// Wildcard ' . ', substitute for any 1 character.
+myString = "I will hum a song about hugging and then hug you."
+myRegex = /hu./g
+result = myString.match(myRegex)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
+
+
+// Match predifined list of characters.
+myString = "Beware of bugs in your bags."
+myRegex = /b[aeiou]gs/g     // Equivalent to /bags|begs|bigs|bogs|bugs/g
+result = myString.match(myRegex)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
+
+
+// Range of letters
+myString = "I am a programmer boy."
+myRegex = /[a-z]/ig     // Equivalent to /[a-z][A-Z]/g
+result = myString.match(myRegex)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
+
+
+// Range of numbers & characters.
+myString = "3.1415 is approx PI."
+myRegex = /[2-4a-p]/ig
+result = myString.match(myRegex)
+
+console.log(myString + '\n' + myRegex + '\n' + result + '\n')
